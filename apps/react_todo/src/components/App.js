e87f066567e2236feb14a4e1552060017e36fe8b
day0ff <day85@tut.by>
2019-03-29 10:51:46 +0300

    Initialization of the project 'Todo React App'.

---
+++ apps/react_todo/src/components/App.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react';
+
+class App extends PureComponent {
+
+  render() {
+    return (
+      <div className="app-container">
+          Todo React App
+      </div>
+    );
+  }
+}
+
+export default App;
